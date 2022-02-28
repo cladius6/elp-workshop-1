4aa920eb7241e72cc123656d221001d6cf43feaf
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-02-28 20:48:23 +0100

    operations object and Operations interface added

--- src/rpn/rpn.ts
+++ src/rpn/rpn.ts
@@ -3,16 +3,27 @@ export function rpn(inputString: string): number {
 
   const stack: number[] = [];
 
+  interface Operations {
+    [operator : string] : (a: number, b: number) => number;
+  }
+
+  const operations: Operations = {
+    '*': (a, b) => a * b,
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '/': (a, b) => a / b, 
+  }
+
   tokens.forEach((token) => {
     const t = Number(token);
 
     if (!isNaN(t)) {
       stack.push(t);
-    } else {
+    } else if (Object.keys(operations).includes(token)){
       const [a, b] = [stack.pop(), stack.pop()]
+      stack.push(operations[token](Number(b), Number(a)))
     }
   });
-
 
   return stack[0];
 }
